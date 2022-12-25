b40cbb4106d9959e9d10738e4c77f30607fd2f42
Pat <pwblais@gmail.com>
2022-12-25 12:32:52 -0500

    fix

--- src/utils/getBuildingData.js
+++ src/utils/getBuildingData.js
@@ -2,7 +2,7 @@ import Web3 from "web3";
 import BuildingManager from "../contracts/BuildingManager.json";
 
 export const getBuildingData = async (tokenId) => {
-	const web3 = new Web3("ws://localhost:8545");
+	const web3 = new Web3("wss://polygon-mumbai.g.alchemy.com/v2/OWb3U7rJFrgVFsYXrGu-h0LId9ki14_V");
 	const BuildingManagerContract = new web3.eth.Contract(
 		BuildingManager.abi,
 		BuildingManager.address
